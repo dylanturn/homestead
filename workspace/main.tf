c91213a75cce26e6e00b07f4cc2445c5a30cac9a
Dylan Turnbull <dylanturn@gmail.com>
2020-09-25 15:20:34 -0500

    use TF module

--- workspace/main.tf
+++ workspace/main.tf
@@ -1,4 +1,12 @@
+#module "argocd" {
+#    source = "git::github.com/dylanturn/terraform-kubernetes-argocd.git?ref=master"
+#    namespace = "argocd-test"
+#}
+
+
 module "argocd" {
-    source = "git::github.com/dylanturn/terraform-kubernetes-argocd.git?ref=master"
-    namespace = "argocd-test"
+  source  = "app.terraform.io/turnbros/argocd/kubernetes"
+  version = "0.0.1"
+  namespace = "argocd-test"
 }
+
