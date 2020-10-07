fd76e0f76673c62236cda24ec22c6ec893fb9b9b
Dylan Turnbull <dylanturn@gmail.com>
2020-10-07 15:00:32 -0500

    added outputs for more remote state developing

--- workspaces/infra-prd-do-nyc3/digital-ocean/outputs.tf
+++ workspaces/infra-prd-do-nyc3/digital-ocean/outputs.tf
@@ -8,3 +8,12 @@ output "cluster_token" {
   value     = module.kubernetes_cluster.cluster_token
   sensitive = true
 }
+output "cluster_cert_issuer" {
+  value = var.cert_manager == null ? "" : module.cert_manager[0].cert_issuer
+}
+output "cluster_ingress_class" {
+  value = var.traefik == null ? "" : module.traefik[0].ingress_class
+}
+output "cluster_argocd_namespace" {
+  value = module.kubernetes_cluster_services.argocd_namespace
+}
--- workspaces/infra-prd-do-nyc3/outputs.tf
+++ workspaces/infra-prd-do-nyc3/outputs.tf
@@ -8,3 +8,12 @@ output "cluster_token" {
   value     = module.digital_ocean.cluster_token
   sensitive = true
 }
+output "cluster_cert_issuer" {
+  value = module.digital_ocean.cluster_cert_issuer
+}
+output "cluster_ingress_class" {
+  value = module.digital_ocean.cluster_ingress_class
+}
+output "cluster_argocd_namespace" {
+  value = module.digital_ocean.cluster_argocd_namespace
+}
