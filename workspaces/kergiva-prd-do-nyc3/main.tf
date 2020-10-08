3246753c11dba8c75e97e29389fb528b2673f034
Dylan Turnbull <dylanturn@gmail.com>
2020-10-08 09:35:30 -0500

    bump chart version

--- workspaces/kergiva-prd-do-nyc3/main.tf
+++ workspaces/kergiva-prd-do-nyc3/main.tf
@@ -40,7 +40,7 @@ module "kergiva_web" {
   project_name      = module.project.name
   namespace         = kubernetes_namespace.rational_healthcare.metadata.0.name
   domain_name       = var.kergiva_web_domain_name
-  chart_version     = "0.1.3"
+  chart_version     = "0.1.4"
   image_repo        = var.kergiva_web_image_repo
   image_name        = var.kergiva_web_image_name
   image_tag         = var.kergiva_web_image_tag
@@ -58,7 +58,7 @@ module "kergiva_api" {
   project_name      = module.project.name
   namespace         = kubernetes_namespace.rational_healthcare.metadata.0.name
   domain_name       = var.kergiva_api_domain_name
-  chart_version     = "0.1.3"
+  chart_version     = "0.1.4"
   image_repo        = var.kergiva_api_image_repo
   image_name        = var.kergiva_api_image_name
   image_tag         = var.kergiva_api_image_tag
@@ -76,7 +76,7 @@ module "kergiva_app" {
   project_name      = module.project.name
   namespace         = kubernetes_namespace.rational_healthcare.metadata.0.name
   domain_name       = var.kergiva_app_domain_name
-  chart_version     = "0.1.3"
+  chart_version     = "0.1.4"
   image_repo        = var.kergiva_app_image_repo
   image_name        = var.kergiva_app_image_name
   image_tag         = var.kergiva_app_image_tag
