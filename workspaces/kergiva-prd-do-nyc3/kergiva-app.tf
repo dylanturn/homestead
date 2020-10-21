5ae37cab7e7f4c1c19a6db61812b85b62402c88a
Dylan Turnbull <dylanturn@gmail.com>
2020-10-21 15:43:20 -0500

    pipe the api endpoint in to into the app endpoint info

--- workspaces/kergiva-prd-do-nyc3/kergiva-app.tf
+++ workspaces/kergiva-prd-do-nyc3/kergiva-app.tf
@@ -6,12 +6,13 @@ module "kergiva_app" {
   cluster_ingress_class    = data.terraform_remote_state.infra.outputs.cluster_ingress_class
   cluster_argocd_namespace = data.terraform_remote_state.infra.outputs.cluster_argocd_namespace
 
-  project_name      = module.project.name
-  namespace         = kubernetes_namespace.rational_healthcare.metadata.0.name
-  domain_name       = var.kergiva_app_domain_name
-  chart_version     = "0.1.6"
-  image_repo        = var.kergiva_app_image_repo
-  image_name        = var.kergiva_app_image_name
-  image_tag         = var.kergiva_app_image_tag
-  image_pull_secret = kubernetes_secret.secret.metadata.0.name
+  project_name         = module.project.name
+  namespace            = kubernetes_namespace.rational_healthcare.metadata.0.name
+  domain_name          = var.kergiva_app_domain_name
+  chart_version        = "0.1.6"
+  image_repo           = var.kergiva_app_image_repo
+  image_name           = var.kergiva_app_image_name
+  image_tag            = var.kergiva_app_image_tag
+  image_pull_secret    = kubernetes_secret.secret.metadata.0.name
+  api_service_endpoint = module.kergiva_api.endpoint
 }
