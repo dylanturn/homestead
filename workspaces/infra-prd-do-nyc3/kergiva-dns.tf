2cd2e6682f3a890a08964a389764fb62125c41df
Dylan Turnbull <dylanturn@gmail.com>
2020-11-03 09:51:53 -0600

    added a root record to kergiva

--- workspaces/infra-prd-do-nyc3/kergiva-dns.tf
+++ workspaces/infra-prd-do-nyc3/kergiva-dns.tf
@@ -9,12 +9,14 @@ resource "digitalocean_record" "kergiva_app_primary_record" {
   domain = digitalocean_domain.kergiva_app.name
   name   = "@"
   type   = "A"
+  ttl    = 300
   value  = module.digital_ocean.cluster_load_balancer
 }
 resource "digitalocean_record" "kergiva_app_cluster_record" {
   domain = digitalocean_domain.kergiva_app.name
   name   = "*"
   type   = "A"
+  ttl    = 300
   value  = module.digital_ocean.cluster_load_balancer
 }
 
@@ -29,11 +31,13 @@ resource "digitalocean_record" "kergiva_org_primary_record" {
   domain = digitalocean_domain.kergiva_org.name
   name   = "@"
   type   = "A"
+  ttl    = 300
   value  = module.digital_ocean.cluster_load_balancer
 }
 resource "digitalocean_record" "kergiva_org_cluster_record" {
   domain = digitalocean_domain.kergiva_org.name
   name   = "*"
   type   = "A"
+  ttl    = 300
   value  = module.digital_ocean.cluster_load_balancer
 }
